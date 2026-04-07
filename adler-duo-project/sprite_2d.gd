ae85000e01f7a4e878d0928549ed23532016804e
joDis06 <jdisomma451@gmail.com>
2026-04-07 02:44:27 -0400

    add resting forward, left, & right sprites; add into project

--- adler-duo-project/sprite_2d.gd
+++ adler-duo-project/sprite_2d.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
 var speed = 400
 var screen_size
@@ -6,6 +6,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
